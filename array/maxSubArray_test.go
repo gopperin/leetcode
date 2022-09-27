f99d7cec19d18e2a5533b06b7bc2bf92b0b98f6b
eric <ministor@126.com>
2022-09-27 18:16:27 +0800

    LargestRectangleArea

--- array/maxSubArray_test.go
+++ array/maxSubArray_test.go
@@ -2,27 +2,7 @@ package array
 
 import (
 	"fmt"
-	"testing"
 )
-
-func ExampleMaxSubArray() {
-
-	ret := SolveNQueens(4)
-
-	fmt.Println(ret)
-
-	// Output:
-	// [[.Q.. ...Q Q... ..Q.] [..Q. Q... ...Q .Q..]]
-}
-
-func BenchmarkMaxSubArray(b *testing.B) {
-
-	for i := 0; i < b.N; i++ {
-		SolveNQueens(4)
-	}
-
-	// 473656	      2529 ns/op	     432 B/op	      28 allocs/op
-}
 
 func ExampleMaxSubArray1() {
 
@@ -34,13 +14,4 @@ func ExampleMaxSubArray1() {
 
 	// Output:
 	// 6
-}
-
-func BenchmarkMaxSubArray1(b *testing.B) {
-
-	for i := 0; i < b.N; i++ {
-		SolveNQueens2(4)
-	}
-
-	// 804691	      1759 ns/op	     328 B/op	      14 allocs/op
 }
